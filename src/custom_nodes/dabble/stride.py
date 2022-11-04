cbcf1f356e37c3e6ff8e0309c696aee15928cbf8
Karthik Prathaban <karthik_prathaban@aiap.sg>
2022-11-04 18:58:15 +0800

    Pipeline with stride node completed

---
+++ src/custom_nodes/dabble/stride.py
@@ -0,0 +1,106 @@
+"""
+Node template for creating custom nodes.
+"""
+
+from typing import Any, Dict, List, Tuple
+import cv2
+
+from peekingduck.pipeline.nodes.abstract_node import AbstractNode
+from runner_utils.mapping import map_bbox_to_image_coords, map_keypoint_to_image_coords
+
+# setup global constants
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+WHITE = (255, 255, 255)       # opencv loads file in BGR format
+YELLOW = (0, 255, 255)
+PURPLE = (255, 0, 255)
+BLUE = (255, 0, 0)
+RED = (0, 0, 255)
+THRESHOLD = 0.6               # ignore keypoints below this threshold
+KP_LEFT_ANKLE = 15         # PoseNet's skeletal keypoints
+KP_RIGHT_ANKLE = 16
+
+def draw_text(img, x, y, text_str: str, color_code):
+   """Helper function to call opencv's drawing function,
+   to improve code readability in node's run() method.
+   """
+   cv2.putText(
+      img=img,
+      text=text_str,
+      org=(x, y),
+      fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+      fontScale=0.4,
+      color=color_code,
+      thickness=2,
+   )
+
+class Node(AbstractNode):
+    """This is a template class of how to write a node for PeekingDuck.
+
+    Args:
+        config (:obj:`Dict[str, Any]` | :obj:`None`): Node configuration.
+    """
+
+    def __init__(self, config: Dict[str, Any] = None, **kwargs: Any) -> None:
+        super().__init__(config, node_path=__name__, **kwargs)
+        self.stride = None
+        # setup object working variables
+
+    def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:  # type: ignore
+        """This node draws keypoints.
+
+        Args:
+            inputs (dict): Dictionary with keys 
+                "img", "bboxes", "bbox_scores", "keypoints", "keypoint_scores".
+
+        Returns:
+            outputs (dict): Empty dictionary.
+        """
+        
+        # get required inputs from pipeline
+        img = inputs["img"]
+        bboxes = inputs["bboxes"]
+        keypoints = inputs["keypoints"]
+        keypoint_scores = inputs["keypoint_scores"]
+
+        img_size = (img.shape[1], img.shape[0])  # image width, height
+
+        # get bounding box confidence score and draw it at the
+        # left-bottom (x1, y2) corner of the bounding box (offset by 30 pixels)
+        try: 
+            the_bbox = bboxes[0]             # image only has one person
+            
+            x1, y1, x2, y2 = map_bbox_to_image_coords(the_bbox, img_size)
+            width, height = img_size[0], img_size[1]
+
+            the_keypoints = keypoints[0]              # image only has one person
+            the_keypoint_scores = keypoint_scores[0]  # only one set of scores
+            right_ankle = None
+            left_ankle = None
+            
+            for i, keypoints in enumerate(the_keypoints):
+                keypoint_score = the_keypoint_scores[i]
+            
+                if keypoint_score >= THRESHOLD:
+                    x, y = map_keypoint_to_image_coords(keypoints.tolist(), img_size)
+                    x_y_str = f"({x}, {y})"
+                                    
+                    the_color = WHITE
+                    
+                    if i == KP_LEFT_ANKLE:
+                        right_ankle = keypoints
+                        the_color = RED
+                    if i == KP_RIGHT_ANKLE:
+                        left_ankle = keypoints
+                        the_color = BLUE
+                    
+                    draw_text(img, x, y, x_y_str, the_color)
+            
+            if right_ankle is not None and left_ankle is not None:
+                self.stride = abs(right_ankle[0] - left_ankle[0])
+                
+                stride_str = f"Stride Distance = {self.stride}"
+                the_color = PURPLE
+                draw_text(img, 30, 30, stride_str, the_color)
+        except IndexError:
+            pass
+        return {"stride":self.stride}
---
+++ src/runner_utils/mapping.py
@@ -0,0 +1,46 @@
+from typing import List, Tuple
+
+def map_bbox_to_image_coords(
+   bbox: List[float], image_size: Tuple[int, int]
+) -> List[int]:
+   """First helper function to convert relative bounding box coordinates to
+   absolute image coordinates.
+   Bounding box coords ranges from 0 to 1
+   where (0, 0) = image top-left, (1, 1) = image bottom-right.
+
+   Args:
+      bbox (List[float]): List of 4 floats x1, y1, x2, y2
+      image_size (Tuple[int, int]): Width, Height of image
+
+   Returns:
+      List[int]: x1, y1, x2, y2 in integer image coords
+   """
+   width, height = image_size[0], image_size[1]
+   x1, y1, x2, y2 = bbox
+   x1 *= width
+   x2 *= width
+   y1 *= height
+   y2 *= height
+   return int(x1), int(y1), int(x2), int(y2)
+
+
+def map_keypoint_to_image_coords(
+   keypoint: List[float], image_size: Tuple[int, int]
+) -> List[int]:
+   """Second helper function to convert relative keypoint coordinates to
+   absolute image coordinates.
+   Keypoint coords ranges from 0 to 1
+   where (0, 0) = image top-left, (1, 1) = image bottom-right.
+
+   Args:
+      bbox (List[float]): List of 2 floats x, y (relative)
+      image_size (Tuple[int, int]): Width, Height of image
+
+   Returns:
+      List[int]: x, y in integer image coords
+   """
+   width, height = image_size[0], image_size[1]
+   x, y = keypoint
+   x *= width
+   y *= height
+   return int(x), int(y)
